d1a21099aa24fb63dec9d6ad6adcb914be45b088
Disha Sardana <dishas9@vt.edu>
2022-08-02 16:06:32 -0400

    LHS and RHS prefabs are changed from cube to plane

--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -26,29 +26,30 @@ namespace Scenes
             // pointList = CSVReader.Read("mc1_clean");
             // mc1Data = new Mc1Data("mc1_processed_neg_1");
             
-            /// TODO Why does this exist?
+            // TODO Why does this exist?
             // pointList = CSVReader.Read("storm_day_sym_index"); <- old one
             var stormIndexDataSet = new StormIndexDataSet(stormIndexDataSetName);
 
-            // Left sliding bar handle with line
+            // Left sliding bar handle with data line
             LeftSlidingBarHandleModel leftSlidingBarHandleModel = new LeftSlidingBarHandleModel();
             leftSlidingBarHandleModel.SerialId = 0;
             leftSlidingBarHandleModel.CenterPosition = new Vector3(0f, 10f, 6f);
             leftSlidingBarHandleModel.Scale = new Vector3(1f,0.1f,1f);
-            leftSlidingBarHandleModel.Rotation = new Vector3(90f,180f,0f);
+            leftSlidingBarHandleModel.Rotation = new Vector3(90f,0f,180f);
             leftSlidingBarHandleModel.StormIndexDataSet = stormIndexDataSet;
-            leftSlidingBarHandleModel.Label = "Dst Index (nT) at 00:00 hrs";
+            leftSlidingBarHandleModel.Label = "SYM-H Index (nT) at 00:00:00 hrs";
             leftSlidingBarHandleController = Controller.Instantiate<LeftSlidingBarHandleController>(
                 LeftSlidingBarHandleModel.PrefabName, leftSlidingBarHandleModel, transform);
             
             // Right sliding bar handle
             RightSlidingBarHandleModel rightSlidingBarHandleModel = new RightSlidingBarHandleModel();
             rightSlidingBarHandleModel.SerialId = 0;
-            rightSlidingBarHandleModel.CenterPosition = new Vector3(-4.5f, -6.6f, -58f);
+            //rightSlidingBarHandleModel.CenterPosition = new Vector3(-4.5f, -6.6f, -58f);
+            rightSlidingBarHandleModel.CenterPosition = new Vector3(0f, 10f, -58f);
             rightSlidingBarHandleModel.Scale = new Vector3(1f,0.1f,1f);
-            rightSlidingBarHandleModel.Rotation = new Vector3(90f,180f,0f);
+            rightSlidingBarHandleModel.Rotation = new Vector3(90f,0f,0f);
             rightSlidingBarHandleModel.StormIndexDataSet = stormIndexDataSet;
-            rightSlidingBarHandleModel.Label = "Dst Index (nT) at xx:xx hrs";
+            rightSlidingBarHandleModel.Label = "SYM-H Index (nT) at 23:59:00 hrs";
             rightSlidingBarHandleController = Controller.Instantiate<RightSlidingBarHandleController>(
                 RightSlidingBarHandleModel.PrefabName, rightSlidingBarHandleModel, transform);
             // var transform1 = transform;
@@ -59,7 +60,7 @@ namespace Scenes
             // Earth (sphere) 
             EarthSphereModel earthSphereModel = new EarthSphereModel();
             earthSphereModel.SerialId = 0;
-            earthSphereModel.CenterPosition = new Vector3(-30f, 0f, 0f);
+            earthSphereModel.CenterPosition = new Vector3(-10f, 0f, 20f);
             earthSphereModel.Scale = new Vector3(5f, 5f, 5f);
             earthSphereModel.Rotation = new Vector3(90f, 0f, 90f);
             earthSphereModel.PointList = pointList;
