749e279f6afd00b6b3c2b3712e99494c4269bdf6
SarthakKahaliya <sarthakkahaliya@gmail.com>
2022-11-27 22:00:26 -0500

    New Change Dataset Button create

--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -34,6 +34,11 @@ namespace Scenes
         [SerializeField]
         private GameObject pressableButton2;
 
+        [SerializeField]
+        private GameObject cycleDatasetButton;
+
+
+
 
         public List<int> datasetNum = new List<int>();
         public int clicked = 0;
@@ -245,8 +250,10 @@ namespace Scenes
                 solarCycleImage.SetActive(true);
 
                 pressableButton2.SetActive(false);
+                pressableButton.SetActive(false);
+                cycleDatasetButton.SetActive(true);
 
-                if(soundMuted == 1)
+                if (soundMuted == 1)
                 {
                     solarCycleImage.GetComponent<AudioSource>().enabled = false;
                 }
@@ -492,8 +499,8 @@ namespace Scenes
                     datasetLabelObject.transform.position = new Vector3(1.68f, 0.73f, 0.85f);
                     datasetLabelObject.transform.localEulerAngles = new Vector3(0f, 90f, 0f);
 
-                    pressableButton.transform.position = new Vector3(1.68f, 0.4f, 0.5f);
-                    pressableButton.transform.localEulerAngles = new Vector3(0f, 90f, 0f);
+                    cycleDatasetButton.transform.position = new Vector3(1.68f, 0.4f, 0.5f);
+                    cycleDatasetButton.transform.localEulerAngles = new Vector3(0f, 90f, 0f);
 
                     TextMesh datasetLabel = datasetLabelObject.GetComponent<TextMesh>();
                     datasetLabel.text = "Dataset: " + stormIndexDataSetName.Substring(20, 10);
